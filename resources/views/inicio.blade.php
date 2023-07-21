f1d2eebc2dab72285ce7eecff11ca11720d425fb
mikiAnge <mikiangel_17@hotmail.com>
2023-07-21 09:25:59 -0400

    Rutas

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,29 @@
+@extends('layout.app')
+<!--todo lo que esta dentro del section se visualizara en la pagina de extension-->
+
+@section('contenido')
+
+<div class="container">
+    <h2>Bienvenido a este</h2>
+    <h1>Portal Web</h1>
+    <p>En el mundo cambiante de hoy, es más importante que nunca mantenerse a la vanguardia 
+    en su campo. La formación continua puede ayudarte a hacer precisamente eso. Al tomar 
+    cursos de educación continua, puede aprender nuevas habilidades, mantenerse actualizado 
+    sobre las últimas tendencias y avanzar en su carrera.</p>
+    <h3>Beneficios de la Formación continua:</h3>
+    <p>Hay muchos beneficios para la formación continua. Algunos de los beneficios más comunes incluyen:</p>
+    <ul>
+        <li>Mayor conocimiento y habilidades: Los cursos de educación continua pueden ayudarlo
+        a aprender nuevas habilidades y conocimientos que se pueden aplicar a su trabajo. Esto 
+        puede hacerlo más valioso para su empleador y darle una ventaja competitiva en el mercado 
+        laboral.</li>
+        <li>Manténgase actualizado sobre las tendencias de la industria: los cursos de educación 
+            continua pueden ayudarlo a mantenerse actualizado sobre las últimas tendencias en su 
+            campo. Esto puede ayudarlo a asegurarse de que está utilizando las últimas prácticas y 
+            tecnologías en su trabajo.</li>
+        <li>Avance en su carrera: Los cursos de educación continua pueden ayudarlo a avanzar en su 
+            carrera al brindarle las habilidades y el conocimiento que necesita para pasar a un nuevo 
+            puesto o nivel de responsabilidad.</li>
+    </ul>
+</div>
+@endsection
---
+++ resources/views/widget/navbar.blade.php
@@ -0,0 +1,6 @@
+@section('navbar')
+<nav>
+    <li>Home</li>
+</nav>
+<h1>Texto</h1>
+@endsection
